fbee762091f17dc999a585e11e5f4cb1a2e3ecc4
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-04-03 18:48:57 -0300

    added DOM function

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-// const dummyTransactions = [
-//   { id: 1, text: 'Flower', amount: -20 },
-//   { id: 2, text: 'Salary', amount: 300 },
-//   { id: 3, text: 'Book', amount: -10 },
-//   { id: 4, text: 'Camera', amount: 150 }
-// ];
+const dummyTransactions = [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 },
+];
+
+let transactions = dummyTransactions;
 
 const localStorageTransactions = JSON.parse(
   localStorage.getItem('transactions')
@@ -44,3 +46,35 @@ const addTransaction = e => {
     amount.value = '';
   }
 };
+
+// Add transactions to DOM list
+const addTransactionDOM = transaction => {
+  // Get sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+
+  // Add class based on value
+  item.classList.add(
+    transaction.amount < 0 ? 'tracker__money--minus' : 'tracker__money--plus'
+  );
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="tracker__delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+  `;
+
+  list.appendChild(item);
+};
+
+// Init app
+const init = () => {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+};
+
+init();
